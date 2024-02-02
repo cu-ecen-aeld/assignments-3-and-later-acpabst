08692ec20b3ca7e87ed42f3fe056cf0fff083cc6
Andy Pabst <andy.pabst16@gmail.com>
2024-02-02 02:18:32 +0000

    fix finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -2,21 +2,21 @@
 # Finder
 # This script searches for content within a provided directory
 # Author: Andy Pabst
-# Date 1/30/24
+# Date: 1/30/24
 
-if [ $# != 2 ]
+if ! [ $# -eq 2 ]
 then
     echo "Missing arguments."
     exit 1
 fi
 
-path = $1
-content = $2
+path=$1
+content=$2
 
 if [ -d "$path" ]
 then
-    numFiles = grep -r ${conteent} ${path} -cH | wc -l
-    numlines =  grep -r ${content} ${path} -oc
+    numFiles=$(grep -rm 1 $content $path | wc -l)
+    numLines=$(grep -r $content $path | wc -l)
     echo "The number of files are ${numFiles} and the number of matching lines are ${numLines}"
 else
     echo "Directory provided does not exist."
