14774e19df0ea8554c216dbdd81d3917817a39f9
Andy Pabst <andy.pabst16@gmail.com>
2024-02-08 04:23:45 +0000

    first pass at systemcalls.c

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,12 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+    bool status = system(cmd);
+    if (status == 0) {
+        return true;
+    } else {
+	return false;
+    }
 }
 
 /**
@@ -58,6 +62,24 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    int status;
+    pid_t pid = fork();
+    if (pid == -1) {
+        return false;
+    } else if (pid == 0) {
+        execv(command[0], command+1);
+        return false;
+    }	
+
+    if (waitpid(pid, &status, 0) == 1) {
+        return false;
+    } else if (status == 0) {
+        return true;
+    } else if (status == 1) {
+	return false;
+    } else {
+	return false;
+    }
 
     va_end(args);
 
@@ -92,6 +114,25 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) { perror("open"); abort(); }
+    if (dup2(fd, 1) < 0) { perror("dup2"); abort(); }
+    close(fd);
+   
+    int status;
+    pid_t pid = fork();
+    if (pid == -1) {
+        return -1;
+    } else if (pid == 0) {
+        execv(command[0], command+1);
+        return -1;
+    }
+
+    if (waitpid(pid, &status, 0) == 1) {
+        return -1;
+    } else if (WIFEXITED(status)) {
+        return WEXITSTATUS(status);
+    }
 
     va_end(args);
 
