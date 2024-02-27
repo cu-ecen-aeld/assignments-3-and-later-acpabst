abe888d59c214bfa0aad5a1c4434f92c543b50cb
Andy Pabst <andy.pabst16@gmail.com>
2024-02-27 22:35:14 +0000

    Assignment 3.2 completion, clean up

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -34,7 +34,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    echo "Kernel Build"
     sudo apt update
     sudo apt-get install -y --no-install-recommends bc u-boot-tools kmod cpio flex bison libssl-dev psmisc libncurses-dev libelf-dev
     sudo apt-get install -y qemu-system-arm
@@ -52,10 +52,6 @@ then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
-
-# TODO: Create necessary base directories
-cd "$OUTDIR"
-
 mkdir -p rootfs
 cd rootfs
 mkdir -p bin dev etc home lib lib64 proc root sbin sys tmp usr var
@@ -63,30 +59,25 @@ mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 cd ..
 
+echo "Make and install busybox"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
     make distclean
     make defconfig
 else
     cd busybox
 fi
-
-# TODO: Make and install busybox
 make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
 make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
-cd ../rootfs
-pwd
-ls
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
 echo `${CROSS_COMPILE}gcc -print-sysroot`
 CROSS_COMPILE_PATH=$(${CROSS_COMPILE}gcc -print-sysroot)
 sudo cp ${CROSS_COMPILE_PATH}/lib/ld-linux-aarch64.so.1 lib/ld-linux-aarch64.so.1
@@ -95,15 +86,14 @@ sudo cp ${CROSS_COMPILE_PATH}/lib64/libm.so.6 lib64/libm.so.6
 sudo cp ${CROSS_COMPILE_PATH}/lib64/libresolv.so.2 lib64/libresolv.so.2
 sudo cp ${CROSS_COMPILE_PATH}/lib64/libc.so.6 lib64/libc.so.6
 
-# TODO: Make device nodes
+echo "Make device nodes"
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 666 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+echo "Clean and build the writer utility"
 cd ${FINDER_APP_DIR}
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+echo "Copy the finder related scripts and executables over"
 make writer CROSS_COMPILE=gcc
 cp writer ${OUTDIR}/rootfs/home/writer
 
@@ -117,10 +107,10 @@ sed -n 's+../conf/assignment.txt+/conf/assignment.txt+g' ${OUTDIR}/rootfs/home/f
 
 cp autorun-qemu.sh ${OUTDIR}/rootfs/home/autorun-qemu.sh
 
-# TODO: Chown the root directory
+echo "Chown the root directory"
 sudo chown root:root *
 
-# TODO: Create initramfs.cpio.gz
+echo "Create initramfs.cpio.gz"
 cd ${OUTDIR}/rootfs
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 cd ..
