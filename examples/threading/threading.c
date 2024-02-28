9bec13dd8b834563155b78241cda36e696616282
Andy Pabst <andy.pabst16@gmail.com>
2024-02-28 05:00:54 +0000

    first pass, some tests passing

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -4,16 +4,35 @@
 #include <stdio.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
-#define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+//#define DEBUG_LOG(msg,...)
+#define DEBUG_LOG(msg,...) printf("\nthreading: " msg "\n\n" , ##__VA_ARGS__)
+#define ERROR_LOG(msg,...) printf("\nthreading ERROR: " msg "\n\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
 {
-
+    DEBUG_LOG("Madde it here!");
+   
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* data = (struct thread_data *) thread_param;
+    sleep(data->obtain_wait/1000);
+    DEBUG_LOG("Well, I think I waited");
+    int rc = pthread_mutex_lock(&data->mutex);
+    if (rc != 0) {
+        ERROR_LOG("pthread mutex lock failed. Code: %d", rc);
+	data->thread_complete_success = false;
+    }
+    sleep(data->release_wait/1000);
+    rc = pthread_mutex_lock(&data->mutex);
+    if (rc != 0) {
+        ERROR_LOG("pthread mutex unlock failed. Code: %d", rc);
+	data->thread_complete_success = false;
+    }
+
+    if (rc == 0) {
+        data->thread_complete_success = true;
+    }	
+    
     return thread_param;
 }
 
@@ -28,6 +47,33 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data *data = (thread_data*)malloc(sizeof(thread_data));
+    data->obtain_wait = wait_to_obtain_ms;
+    data->release_wait = wait_to_release_ms;
+
+    //int rc = 0;
+    data->thread_complete_success = false;
+    
+    // Set up Mutex
+    int rc = pthread_mutex_init(&data->mutex,NULL);
+    if (rc != 0) {
+        ERROR_LOG("pthread mutex init failed. Code: %d", rc);
+	return false;
+    }
+    //rc = pthread_mutex_lock(&data->mutex);
+    if (rc != 0) {
+        ERROR_LOG("pthread mutex init failed. Code: %d", rc);
+        return false;
+    }
+    // create thread
+    rc = pthread_create(thread, NULL, threadfunc, &data);
+    if (rc != 0) {
+	ERROR_LOG("thread could not be created. Code: %d", rc);
+    }
+    
+    DEBUG_LOG("Thread complete status: %d", data->thread_complete_success);
+    free(data);
+    return true;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -7,7 +7,7 @@
  * It should be returned by your thread so it can be freed by
  * the joiner thread.
  */
-struct thread_data{
+typedef struct thread_data {
     /*
      * TODO: add other values your thread will need to manage
      * into this structure, use this structure to communicate
@@ -16,11 +16,19 @@ struct thread_data{
      */
 
     /**
+     * The mutex used to lock this data when manipulating values, for thread safety
+     */
+    pthread_mutex_t mutex;
+
+    int obtain_wait;
+    int release_wait;
+
+    /**
      * Set to true if the thread completed with success, false
      * if an error occurred.
      */
     bool thread_complete_success;
-};
+} thread_data;
 
 
 /**
