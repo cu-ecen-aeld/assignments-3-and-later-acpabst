dfcbb7d7bc15b9a0317a66a8935017f769537ee7
Andy Pabst <andy.pabst16@gmail.com>
2024-04-16 02:31:22 +0000

    progress

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -23,13 +23,47 @@
 #  define PDEBUG(fmt, args...) /* not debugging: nothing */
 #endif
 
+#define AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED 10
+
+struct aesd_buffer_entry
+{
+    /**
+     * A location where the buffer contents in buffptr are stored
+     */
+    const char *buffptr;
+    /**
+     * Number of bytes stored in buffptr
+     */
+    size_t size;
+};
+
 struct aesd_dev
 {
     /**
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
-    struct cdev cdev;     /* Char device structure      */
+    //mutex* aesd_mutex
+
+    /* The buffer */
+    struct aesd_buffer_entry  entry[AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED];
+    
+    /* Pointer to the next write location */
+    uint8_t in_offs;
+
+    /* Pointer to the beginning of the buffer. Will move upon overwrite */
+    uint8_t out_offs; 	
+    
+    /* Indicates whether the buffer is full */
+    bool full;
+ 
+    /* TODO Partial buffer */    
+    
+    /* Char device structure */
+    struct cdev cdev;     
 };
 
 
-#endif /* AESD_CHAR_DRIVER_AESDCHAR_H_ */
+
+
+#endif /* AESD_
+	  CHAR_DRIVER_AESDCHAR_H_ */
--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -17,14 +17,18 @@
 #include <linux/types.h>
 #include <linux/cdev.h>
 #include <linux/fs.h> // file_operations
+
+#include <linux/mutex.h>
+
 #include "aesdchar.h"
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
-MODULE_AUTHOR("Your Name Here"); /** TODO: fill in your name **/
+MODULE_AUTHOR("Andy Pabst"); /** TODO: fill in your name **/
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
+struct mutex mutex;
 
 int aesd_open(struct inode *inode, struct file *filp)
 {
@@ -32,6 +36,12 @@ int aesd_open(struct inode *inode, struct file *filp)
     /**
      * TODO: handle open
      */
+    struct aesd_dev *dev
+    dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
+    filp->private_data = dev;
+
+    //
+    
     return 0;
 }
 
@@ -52,6 +62,21 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
     /**
      * TODO: handle read
      */
+    // TODO obtain mutex
+    for(uint8 i = filp->private_data->out_offs; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED, i++) {
+       buf[retval] = filp->private_data->entry[i]->buffptr;
+       retval+= filp->private_data->entry[i]->size;
+    }
+    if (filp->private_data->full) {
+        if (filp->private_data->out_offs > 0) {
+	    // circle back
+	    for(unsigned int i = 0, i < filp->private_data->out_offs, i++) {
+	        buf[retval] = filp->private_data->entry[i]->buffptr;
+		retval += filp->private_data->entry[i]->size;
+	    }
+        }
+    }
+    // TODO release mutex
     return retval;
 }
 
@@ -63,6 +88,47 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
     /**
      * TODO: handle write
      */
+    // assume each write will end with a new line character
+    // create entry from buffer and count provided
+    struct aesd_buffer_entry *add_entry;
+    aesd_buffer_entry->size = count;
+    // kmalloc a buffer to store the data we are given
+    char *data = kmalloc(count, GFP_KERNEL);
+    for(unsigned int i = 0; i < count; i++) {
+       // TODO if null character, set flag
+       // 	if the flag is set, write to buffer, else write to partial buffer
+       data[i] = buf[i];
+    } 
+    add_entry->buffptr = data;
+    // TODO obtain mutex here
+    // add created entry to the buffer
+    filp->private_data->entry[in_offs] = add_entry;
+    if(flip->private_date->full) {
+        // buffer is full, we are overwriting an entry
+	// advance both in_off and out_offs
+        if(filp->private_data->in_offs + 1 == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
+            // reached the end of the buffer again, start at the beginning
+            filp->private_data->in_offs = 0;
+        } else {
+            filp->private_data->in_offs++;
+        }
+        if(filp->private_data->out_offs + 1 == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
+	    filp->private_data->out_offs = 0;
+	} else {
+	    filp->private_data->out_offs++;
+	}
+    } else {
+	// buffer is not full, increment in_offs only
+	if (filp->private_data->in_offs + 1 == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
+            // buffer is now full
+	    filp->private_data->in_offs = 0;
+	    filp->private_data->full = true;
+	} else {
+            filp_private_data->in_offs++;
+	}
+    }
+    // release mutex
+
     return retval;
 }
 struct file_operations aesd_fops = {
@@ -105,7 +171,15 @@ int aesd_init_module(void)
     /**
      * TODO: initialize the AESD specific portion of the device
      */
-
+    // init locking primitive
+    // allocate memory if needed - do I need to allocate filp->private_data. See scull?
+    //mutex_init(&aesd_mutex);
+    //aesd_device.aesd_mutex = *mutex;
+    
+    aesd_device->full = false;
+    aesd_device->in_offs = 0;
+    aesd_device->read_ptr = 0;
+     
     result = aesd_setup_cdev(&aesd_device);
 
     if( result ) {
@@ -124,6 +198,9 @@ void aesd_cleanup_module(void)
     /**
      * TODO: cleanup AESD specific poritions here as necessary
      */
+    // free memory if needed
+    // TODO for each entry, free the data
+    free(aesd_device);
 
     unregister_chrdev_region(devno, 1);
 }
