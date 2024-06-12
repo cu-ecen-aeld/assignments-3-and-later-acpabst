7129c61d3899775a11962dcbd1498c961cb35c59
Andy Pabst <andy.pabst16@gmail.com>
2024-06-12 04:28:16 +0000

    LDFLAGS

--- aesd-char-driver/run_flush.sh
+++ aesd-char-driver/run_flush.sh
@@ -7,10 +7,10 @@ sudo ./../aesd-char-driver/aesdchar_unload
 sudo ./../aesd-char-driver/aesdchar_load
 
 # write data to the circular buffer
-echo "write1" > ${device}
-echo "write2" > ${device}
-echo "write3" > ${device}
-echo "write4" > ${device}
+echo "write1" >> ${device}
+echo "write2" >> ${device}
+echo "write3" >> ${device}
+echo "write4" >> ${device}
 echo "Contents of aesdchar:"
 cat ${device}
 
